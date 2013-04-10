c92915b0094d6db047b380dcfbbd15317634aec9
Masayuki Ataka <ataka@actindi.net>
2013-04-10 09:54:01 +0900

    initial commit

---
+++ lib/rubygems/format/dummy.rb
@@ -0,0 +1,9 @@
+require "rubygems/format/dummy/version"
+
+module Rubygems
+  module Format
+    module Dummy
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rubygems/format/dummy/version.rb
@@ -0,0 +1,7 @@
+module Rubygems
+  module Format
+    module Dummy
+      VERSION = "0.0.1"
+    end
+  end
+end
